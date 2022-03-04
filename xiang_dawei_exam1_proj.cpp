0a940bf87c57c17081a2badaab4337baced86c05
yoshua133 <1332803264@qq.com>
2022-03-04 17:08:26 -0600

    Update xiang_dawei_exam1_proj.cpp

--- xiang_dawei_exam1_proj.cpp
+++ xiang_dawei_exam1_proj.cpp
@@ -131,81 +131,25 @@ void LRU(){
 
 
 void second(){
-    deque<int> cur_pages;
-    deque<int> removed;
-    queue_print(cur_pages,removed);
-    for (int i =0; i<24; i++){
-        int cur  = reference_string[i];
-        int result = check(cur_pages,cur);
-        if ( cur_pages.size()<4){
-                cur_pages.push_back(cur);
-                queue_print(cur_pages,removed, cur, result);
-                continue;
-            }
-
-        if  (result<0){
-            
-            int cur_removed = cur_pages.front();
-            cur_pages.pop_front();
-            
-            cur_pages.push_back(cur);
-            if ( removed.size()<4){
-                removed.push_back(cur_removed);
-            }
-            else{
-                removed.pop_front();
-                removed.push_back(cur_removed);
-            }
-        }
-        else{
-            deque<int>::iterator it = cur_pages.begin() ;
-            it += result;
-            cur_pages.erase(it);
-            cur_pages.push_back(cur);
-
-        }
-        queue_print(cur_pages,removed, cur, result);
-
-    }
-}
 //     deque<int> cur_pages;
-//     deque<int> signs; // sign for whether referenced
 //     deque<int> removed;
 //     queue_print(cur_pages,removed);
-//     int cur_pointer = 0;
 //     for (int i =0; i<24; i++){
-//         int cur_num  = reference_string[i];
-//         int result = check(cur_pages,cur_num);
+//         int cur  = reference_string[i];
+//         int result = check(cur_pages,cur);
 //         if ( cur_pages.size()<4){
-//                 cur_pages.push_back(cur_num);
-//                 signs.push_back(0);
-//                 queue_print(cur_pages,removed, cur_num, result);
+//                 cur_pages.push_back(cur);
+//                 queue_print(cur_pages,removed, cur, result);
 //                 continue;
 //             }
+
 //         if  (result<0){
-//             while(signs[cur_pointer]==1){
-//                 signs[cur_pointer]=0;
-//                 cur_pointer ++;
-//                 if (cur_pointer>= signs.size()){
-//                     cur_pointer = 0;
-//                 }
-//             }
-//             int cur_removed = cur_pages[cur_pointer];
-
-
-//             deque<int>::iterator it1 = cur_pages.begin() ;
-//             deque<int>::iterator it2 = signs.begin() ;
-//             it1 += cur_pointer;
-//             it2 += cur_pointer;
-//             cur_pages.erase(it1);
-//             //cout<<cur_num;
-//             it1 = cur_pages.insert(it1,2);
-//             cur_pages.insert(it1,20);
-//             cur_pages.insert(it1,200);
-//             signs.erase(it2);
-//             signs.insert(it2,0);
             
-//             if (removed.size()<4){
+//             int cur_removed = cur_pages.front();
+//             cur_pages.pop_front();
+            
+//             cur_pages.push_back(cur);
+//             if ( removed.size()<4){
 //                 removed.push_back(cur_removed);
 //             }
 //             else{
@@ -214,13 +158,68 @@ void second(){
 //             }
 //         }
 //         else{
-//             signs[result] = 1;
+//             deque<int>::iterator it = cur_pages.begin() ;
+//             it += result;
+//             cur_pages.erase(it);
+//             cur_pages.push_back(cur);
 
 //         }
-//         queue_print(cur_pages,removed, cur_num, result);
+//         queue_print(cur_pages,removed, cur, result);
 
 //     }
 // }
+    deque<int> cur_pages;
+    deque<int> signs; // sign for whether referenced
+    deque<int> removed;
+    queue_print(cur_pages,removed);
+    int cur_pointer = 0;
+    for (int i =0; i<24; i++){
+        int cur_num  = reference_string[i];
+        int result = check(cur_pages,cur_num);
+        if ( cur_pages.size()<4){
+                cur_pages.push_back(cur_num);
+                signs.push_back(0);
+                queue_print(cur_pages,removed, cur_num, result);
+                continue;
+            }
+        if  (result<0){
+            while(signs[cur_pointer]==1){
+                signs[cur_pointer]=0;
+                cur_pointer ++;
+                if (cur_pointer>= signs.size()){
+                    cur_pointer = 0;
+                }
+            }
+            int cur_removed = cur_pages[cur_pointer];
+
+
+            cur_pages.erase(cur_pages.begin() +cur_pointer);
+            //cout<<cur_num;
+            cur_pages.insert(cur_pages.begin()+cur_pointer ,cur_num);
+            //it1 = cur_pages.insert(it1,20);
+            //it1 = cur_pages.insert(it1,200);
+            signs.erase(signs.begin() + cur_pointer);
+            signs.insert(signs.begin() + cur_pointer,0);
+            cur_pointer ++;
+            if (cur_pointer>= signs.size()){
+                    cur_pointer = 0;
+            }
+            if (removed.size()<4){
+                removed.push_back(cur_removed);
+            }
+            else{
+                removed.pop_front();
+                removed.push_back(cur_removed);
+            }
+        }
+        else{
+            signs[result] = 1;
+
+        }
+        queue_print(cur_pages,removed, cur_num, result);
+
+    }
+}
 
 
 int main(){
